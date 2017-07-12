0ef746cafea35906ec046e2dfe14319b9f97ff2f
InnovateBham_10 <InnovateBham_10@Admins-iMac.local>
2017-07-12 16:08:03 -0500

    first commit

---
+++ list.js
@@ -0,0 +1,10 @@
+
+
+        $.getJSON("http://www.reddit.com/r/StarWars/.json?jsonp=?", function(data) {
+    $.each(data.data.children, function(i,item){
+        $("<img/>").attr("src", item.data.url).appendTo("#images");
+    });
+});
+
+var heading = document.createElement('h2');
+heading.innertext = post.data.tile 
--- script.js
+++ script.js
@@ -25,13 +25,29 @@
 //     xmlHttp.open("GET", true); // true for asynchronous 
 //     xmlHttp.send(null);
 // }
-function show_image(src, width, height, alt) {
-    var img = document.createElement("img");
-    img.src = src;
-    img.width = width;
-    img.height = height;
-    img.alt = alt;
+// function show_image(src, width, height, alt) {
+//     var img = document.createElement("img");
+//     img.src = src;
+//     img.width = width;
+//     img.height = height;
+//     img.alt = alt;
 
-    // This next line will just add it to the <body> tag
-    document.body.appendChild(img);
-}
+//     // This next line will just add it to the <body> tag
+//     document.body.appendChild(img);
+// }
+
+// window.onload = function(){
+//     var http = new XMLHttpRequest();
+//     http.open("GET" , "http://i.imgur.com/uos55n1.jpg" , true);
+//     http.send();
+//     console.log(http);
+// }
+
+$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=cat&tagmode=any&format=json&jsoncallback=?",
+        function(data){
+          $.each(data.items, function(i,item){
+            $("<img/>").attr("src", item.media.m).appendTo("#images");
+            
+          });
+        });
+  
