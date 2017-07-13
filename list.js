3cff02403d30a0b4bbe44d0d7edc121e1c71004c
InnovateBham_10 <InnovateBham_10@Admins-iMac.local>
2017-07-13 11:57:41 -0500

    needs link

--- list.js
+++ list.js
@@ -1,10 +1,57 @@
+// $.getJSON("http://www.reddit.com/r/StarWars/.json?jsonp=?", function(data) {
+//     $.each(data.data.children, function(i,item){
+//         $("<img/>").attr("src", item.data.url).appendTo("#images");
+//     });
+
+// $.getJSON("https://www.reddit.com/r/StarWars/.json?jsonp=?",
+//        function subRed(data) {
+//            $.each(data.data.children.slice(0, 20),
+//            function (i, item) {
+//              $("#reddit-content").append( '<br>' + item.data.title );
+//              $("#reddit-content").append( '<br>' + item.data.url );            
+//              $("#reddit-content").append( item.data.title + item.data.url ); //Trying to turn post title into link
+//              $("<img/>").attr("src", item.data.url).appendTo("#reddit-content");
+//              $("#reddit-content").append( '<hr>' );
+//            }
+//          )
+//        }
+//      )
 
 
-        $.getJSON("http://www.reddit.com/r/StarWars/.json?jsonp=?", function(data) {
-    $.each(data.data.children, function(i,item){
-        $("<img/>").attr("src", item.data.url).appendTo("#images");
-    });
-});
 
-var heading = document.createElement('h2');
-heading.innertext = post.data.tile;  
+
+var httpRequest = new XMLHttpRequest();
+var star = "http://www.reddit.com/r/art.json";
+
+httpRequest.open("GET", star);
+httpRequest.send();
+httpRequest.addEventListener("readystatechange", processRequest);
+function processRequest(e) {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+        var data = e.target.response;
+        var parsedData = JSON.parse(data);
+
+        var posts = parsedData.data.children;
+
+        posts.forEach(function(post) {
+            var heading = document.createElement('h2');
+            heading.innerText = post.data.title;
+            document.body.appendChild(heading);
+            var postBody = document.createElement('img');
+            postBody.src = post.data.url;
+            document.body.appendChild(postBody);
+
+        var link = document.createElement('a')
+       link.setAttribute('href', 'single.html', + '?link=' + post.data.permalink );
+       link.style.width = '300px';
+       document.body.appendChild(link);
+       link.appendChild(heading);
+        });
+
+
+        
+    }
+
+}
+
+
--- script.js
+++ script.js
@@ -43,11 +43,11 @@
 //     console.log(http);
 // }
 
-$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=cat&tagmode=any&format=json&jsoncallback=?",
-        function(data){
-          $.each(data.items, function(i,item){
-            $("<img/>").attr("src", item.media.m).appendTo("#images");
+// $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=cat&tagmode=any&format=json&jsoncallback=?",
+//         function(data){
+//           $.each(data.items, function(i,item){
+//             $("<img/>").attr("src", item.media.m).appendTo("#images");
             
-          });
-        });
+//           });
+//         });
   
